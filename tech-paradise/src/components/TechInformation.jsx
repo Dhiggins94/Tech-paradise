b091fe9fb890d1557e2aa4ccc84bf42061b24b89
divine higgins <divinehiggins@divines-MacBook-Pro.local>
2021-07-15 15:24:07 -0400

    filled out 2nd issue ticket, i can now map through the second airtable

--- tech-paradise/src/components/TechInformation.jsx
+++ tech-paradise/src/components/TechInformation.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { useEffect,useState } from 'react'
 import axios from 'axios'
 import { useParams } from 'react-router'
-import { useHistory } from "react-router";
-import {ReactStars} from "react-rating-stars-component";
+// import { useHistory } from "react-router";
+import StarRating from 'react-star-rating'
 
 const AIRTABLE_KEY = process.env.REACT_APP_AIRTABLE_KEY
 const AIRTABLE_BASE = process.env.REACT_APP_AIRTABLE_BASE
@@ -11,42 +11,50 @@ const PRODUCT_BASE_URL = `https://api.airtable.com/v0/${AIRTABLE_BASE}/products`
 const REVIEW_BASE_URL = `https://api.airtable.com/v0/${AIRTABLE_BASE}/reviews`
 const headers = { Authorization: `Bearer ${AIRTABLE_KEY}` }
 
-const forms = {
-  name: "",
-  review: ""
-}
 export default function TechInformation() {
   const [device, setDevice] = useState({})
+  const [ratings, setRatings] = useState([])
+  const [allReviews, setAllReviews] = useState([])
+  const [productReviews, setProductReviews] = useState([])
   const { id } = useParams()
-  const [input, setInput] = useState(forms)
-  const history = useHistory()
+  // const [input, setInput] = useState(forms)
+  // const history = useHistory()
 
-// handles table 1 axios call
+// handles table 1 axios  get call
   useEffect(() => {
-  const fetchDevice = async () => {
-  const deviceURL = `${PRODUCT_BASE_URL}/${id}`
-  const res = await axios.get(deviceURL, { headers })
-  setDevice(res.data)
-  }
-  fetchDevice()
+    const fetchDevice = async () => {
+      const deviceURL = `${PRODUCT_BASE_URL}/${id}`
+      const res = await axios.get(deviceURL, { headers })
+      setDevice(res.data)
+    }
+    // handles table 1 axios get call
+    const fetchAllReviews = async () => {
+      const reviewTable = `${REVIEW_BASE_URL}`
+      const response = await axios.get(reviewTable, { headers })
+      // setRatings(response.data)
+      setAllReviews(response.data.records)
+    }
+    fetchDevice()
+    fetchAllReviews()
   }, [id])
-
-// the handlechange and submit function
   
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setInput((prevInput) => ({
-      ...prevInput,
-      [name]: value,
-    }));
-  };
-// POST for the 2nd table
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const res = await axios.post(REVIEW_BASE_URL, { fields: input }, { headers });
-    console.log(res);
-    history.push(`/product/${id}`)
-  };
+  useEffect(() => {
+      const fetchProductReviews = async (id) => {
+        const productReviews = allReviews.filter(review => (
+          review.fields.products[0] === id
+        ))
+
+        setProductReviews(productReviews)
+      }
+      if (allReviews.length > 0) {
+        fetchProductReviews(id)
+      }
+    }, [id,allReviews])
+  const reviewsJSX = productReviews.map((review) => {
+    <p> { review.fields.name} </p>
+    })
+  
+
     
   return (
     <div>
@@ -55,34 +63,10 @@ export default function TechInformation() {
       <img src={device.fields?.image} alt="item" />
       <p>{device.fields?.productDescription}</p>
       <p>{device.fields?.price}</p>
-      
-        {/* shows the form and star rating */}
-      <form onSubmit={handleSubmit}>
-        <label>your name</label>
-        <br />
-        <input name="name"
-          value={input.name}
-          onChange={handleChange}
-        placeholder="Your name goes here"
-          />
-          <br />
-        <label> review section</label>
-        <input name="review"
-          value={input.review}
-          onChange={handleChange}
-          placeholder="Your reviews goes here"
-          />
-        <br />
-        <label>Rating</label>      
-        <ReactStars count={5}
-        value={input.rating}
-        onChange={handleChange}
-        size={24}
-        color2={'#ffd700'}
-        />
-        <br />
-        <button>submit review</button>
-      </form>
+      {/* maps through the 2nd table */}
+      {productReviews.map(review => (
+        <p> rating {review.fields.rating} out of 5 review: {review.fields.review} </p>
+      ))}
      
     </div>
   )
