bb9fb4367bbec51508f365ffadc2d731223175c5
divine higgins <divinehiggins@divines-MacBook-Pro.local>
2021-07-18 12:49:47 -0400

    added buttons

--- tech-paradise/src/components/Home.jsx
+++ tech-paradise/src/components/Home.jsx
@@ -1,41 +1,35 @@
 import React from 'react'
 import "./Home.css"
-// import axios from "axios"
-// import { useState,useEffect } from 'react'
-// import { PRODUCT_BASE_URL, headers } from '../services'
-// import { useParams } from 'react-router'
+import axios from "axios"
+import { useEffect } from 'react'
+import { PRODUCT_BASE_URL, headers } from '../services'
 
 
 export default function Home() {
-  // const [category, setCategory] = useState("")
-  
-  // const { id } = useParams
-  
-  // table 1 get request
- 
-  // const handleSubmit = async (event) => {
-  //   event.preventDefault();
-  //   useEffect(() => {
-  //     const fetchCategories = async () => {
-  //       const categoryUrl = `${PRODUCT_BASE_URL}`
-  //       const response = await axios.get(categoryUrl, { headers })
-  //       const filterCategories = response.data.records.filter(category => (
-  //         category.fields.category === id
-  //       ))
-  //       setCategory(filterCategories)
-  //       console.log(response.data)
-  //     }
-  //     fetchCategories()
-  //   },[id])
 
-  //       }
+  // const [filterInput, setFilterInput] = useState("")
+  // const [ filterCategory, setFilterCategory] = useState("")
+// first table 1 get request
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const response = await axios.get(PRODUCT_BASE_URL, { headers })
+      console.log(response.data)
+
+    }
+    fetchCategories()
+},[])
+
+
 
   return (
     <div className="home">
       <p> Welcome to tech-paradise, this site is used to explore and submit reviews of electonics that are on the market</p>
       <p> press the button at the top to see our selection  </p>
-      <input />
-      <button > Search </button>
+      <button > desktops </button>
+      <button > laptops </button>
+      <button > keyboards </button>
+      <button > mouses </button>
+      <button > monitors </button>
     </div>
   )
 }
